df9c2bd4a58077300c35ff5d1819b06880899470
Phourtie-Phour <114376783+Phourtie-Phour@users.noreply.github.com>
2024-03-30 11:12:50 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-    document.querySelector('h2').innerHTML = "ボタンをクリックしました";
-    console.log('クリックされました!');
-});
